000135485ec3551b262233bef681c9e220986f1a
krishnamohan <krishnamohan.py@gmail.com>
2020-09-23 12:49:39 +0530

    cookies & session

---
+++ src/CookieController.java
@@ -0,0 +1,34 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class CookieController
+ */
+@WebServlet("/controller")
+public class CookieController extends HttpServlet {
+       
+  
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		  String name =request.getParameter("name");
+		  String pass =request.getParameter("pass");
+		  //cookie
+		  Cookie ck = new Cookie("Username",name);
+		  response.addCookie(ck);
+		  
+		  //session
+		  HttpSession session =  request.getSession();
+		  session.setAttribute("Password",pass);
+		  response.sendRedirect("result.jsp");
+		
+	}
+
+}
